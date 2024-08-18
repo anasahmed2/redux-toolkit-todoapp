a151723553b82973cf51544502a7adfe382e28a2
anasahmed2 <muhammadanas61222@gmail.com>
2024-08-18 21:51:05 +0500

    create todoapp using redux-toolkit

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { removeTodo } from '../features/todo/todoslice'
 
 function Todos() {
+  const todos = useSelector(state => state.todos)
+  const dispatch = useDispatch()
   return (
-    <div>
-      Todo
-    </div>
+    <>
+      <div>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+            <button
+              onClick={() => dispatch(removeTodo(todo.id))}>X</button>
+          </li>
+        ))}
+      </div>
+    </>
+
   )
 }
 

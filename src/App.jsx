2c2c179a32d118afaab28f7f997dffb02ee454ed
anasahmed2 <muhammadanas61222@gmail.com>
2024-08-22 17:16:18 +0500

    create todoapp using redux-toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import AddTodo from './components/AddTodo'
 import Todos from './components/Todos'
@@ -10,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1>learn about redux toolkit</h1>
+      <h1 className='font-semibold text-lg capitalize'>learn about redux toolkit</h1>
       <AddTodo />
       <Todos />
     </>
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { removeTodo, editeTodo } from '../features/todo/todoslice'
 
@@ -8,15 +8,21 @@ function Todos() {
   const [input, setInput] = useState("")
 
   const dispatch = useDispatch()
+  const inputRef = useRef(null)
 
   const editeTodoHandler = (e, id) => {
     e.preventDefault()
-    if (editingTodoId) {
+    if (editingTodoId ) {
       dispatch(editeTodo({ id, text: input }))
       setEditingTodoId(null)
       setInput("")
     }
   }
+  useEffect(() => {
+    if (editingTodoId !== null && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [editingTodoId]);
 
   const startEditing = (id, currentText) => {
     setEditingTodoId(id)
@@ -25,41 +31,50 @@ function Todos() {
 
   return (
     <>
-      <div>
+      <div className='font-semibold text-lg capitalize'>Todos</div>
+      <ul className='list-none'>
         {todos.map((todo) => (
-          <li key={todo.id}>
+          <li
+            className={`mx-[10px] sm:mx-[40px] mt-4 flex justify-between
+            items-center ${editingTodoId === todo.id ? "bg-gray-800 border border-indigo-500 outline-indigo-900 " : "bg-gray-800 "} px-4 py-2 rounded`}
+            key={todo.id}>
             {editingTodoId === todo.id ? (
-              <input 
-                type='text' 
-                className='w-[100px] h-[30px] bg-red-400' 
-                value={input} 
-                onChange={(e) => setInput(e.target.value)} 
+              <input
+                type='text'
+                ref={inputRef}
+                className={`w-full mr-2 bg-transparent border-none outline-none text-gray-100 py-0 px-0 leading-8 `}
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
               />
             ) : (
-              todo.text
+              <div className='text-white'>
+                {todo.text}
+              </div>
             )}
+            <div className='flex justify-center gap-1'>
+              <button
+                className={`text-white bg-red-500 border-0 py-1 px-4  focus:outline-none hover:bg-red-600 rounded text-md`}
+                onClick={() => dispatch(removeTodo(todo.id))}>
+                delete
+              </button>
+              {editingTodoId === todo.id ? (
+                <button
+                  className={`text-white bg-green-500 border-0 py-1 px-4  focus:outline-none hover:bg-green-600 rounded text-md`}
+                  onClick={(e) => editeTodoHandler(e, todo.id)}>
+                  Save
+                </button>
+              ) : (
+                <button
+                  className={`text-white bg-blue-500 border-0 py-1 px-4  focus:outline-none hover:bg-blue-600 rounded text-md`}
+                  onClick={() => startEditing(todo.id, todo.text)}>
+                  Edit
+                </button>
+              )}
+            </div>
 
-            <button
-              className='py-1 px-6 bg-red-500'
-              onClick={() => dispatch(removeTodo(todo.id))}>
-              X
-            </button>
-            {editingTodoId === todo.id ? (
-              <button
-                className='py-1 px-6 bg-green-500'
-                onClick={(e) => editeTodoHandler(e, todo.id)}>
-                Save
-              </button>
-            ) : (
-              <button
-                className='py-1 px-6 bg-blue-500'
-                onClick={() => startEditing(todo.id, todo.text)}>
-                Edit
-              </button>
-            )}
           </li>
         ))}
-      </div>
+      </ul>
     </>
   )
 }
